1aa44e7afe4cb89402cf4cf7146358f0a35d2409
Randall Holsinger <randallholsinger@gmail.com>
2022-06-30 11:48:48 -0700

    working on register form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,9 +2,53 @@ import React, { useState } from 'react'
 
 function Register() {
 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [confirmEmail, setConfirmEmail] = useState('')
+  //check if input values are valid
+  const [validUsername, setValidUsername] = useState(false)
+  const [validPassword, setValidPassword] = useState(false)
+  const [validEmail, setValidEmail] = useState(false)
+  const [validConfirmEmail, setValidComfirmEmail] = useState(false) 
+
+  const handleRegisterSubmit = () => {
+    
+  }
+
   return(
     <div className="Register">
-      Register
+      <form onSubmit={handleRegisterSubmit}>
+        <label htmlFor="username">Enter Username</label>
+        <input 
+         type="text" 
+         id="username"
+         value={username}
+         onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Enter Password</label>
+        <input 
+         type="password" 
+         id="password"
+         value={password}
+         onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="email">Enter Email</label>
+        <input 
+         type="text" 
+         id="email"
+         value={email}
+         onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="confirm-email">Confirm Email</label>
+        <input 
+         type="text" 
+         id="confirm-email"
+         value={confirmEmail}
+         onChange={(e) => setConfirmEmail(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
     </div>
   )
 }
