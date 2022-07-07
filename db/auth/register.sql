8aa0158e9afa917474b4faa8c7840da9218ebfc0
Randall Holsinger <randallholsinger@gmail.com>
2022-07-07 13:24:16 -0700

    added database files

--- db/auth/register.sql
+++ db/auth/register.sql
@@ -1,3 +1,3 @@
-insert into users(username, password, email)
-values(${username}, ${password}, ${email})
+insert into users(username, password, email, date)
+values(${username}, ${password}, ${email}, now())
 returning user_id , username
