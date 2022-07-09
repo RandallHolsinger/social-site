36146f9381d434d82fa2928dcb9bc972cb6ef9bb
Randall Holsinger <randallholsinger@gmail.com>
2022-07-08 17:35:56 -0700

    Finished adding server endpoints

--- server/server.js
+++ server/server.js
@@ -5,7 +5,12 @@ const app = express()
 const massive = require('massive')
 const session = require('express-session')
 const ctrlAuth = require('./controllers/auth')
-const auth = require('./controllers/auth')
+const ctrlUsers = require('./controllers/users')
+const ctrlFriends = require('./controllers/friends')
+const ctrlPosts = require('./controllers/posts')
+const ctrlComments = require('./controllers/comments')
+const ctrlMessages = require('./controllers/messages')
+const { applyPatches } = require('immer')
 
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 
@@ -26,7 +31,7 @@ massive(CONNECTION_STRING).then(db => {
   app.listen(SERVER_PORT, () => console.log(`Listening On Server Port#: ${SERVER_PORT}`))
 })
 
-// Authentication Endpoints //
+///// Authentication Endpoints /////
 
 //Register
 app.post('/auth/user/register', ctrlAuth.register)
@@ -39,6 +44,75 @@ app.post('/auth/user/logout', ctrlAuth.logout)
 
 //Get Current User
 app.get('/auth/user/current', ctrlAuth.current)
+
+///// Users Endpoint /////
+
+// Get Users
+app.get('/api/users', ctrlUsers.getUsers)
+
+// Update User Personal Profile
+app.put('/api/user/update/:user_id', ctrlUsers.updateUser)
+
+// Delete User Personal Profile
+app.delete('/api/user/delete/:user_id', ctrlUsers.deleteUser)
+
+///// Post's Endpoints /////
+
+// Add Post
+app.post('/api/post/add', ctrlPosts.addPost)
+
+// Delete Post
+app.delete('/api/post/delete/:post_id', ctrlPosts.deletePost)
+
+// Get Posts
+app.get('/api/posts', ctrlPosts.getPosts)
+
+// Edit Post 
+app.put('/api/post/edit/:post_id', ctrlPosts.updatePost)
+
+///// Comment's Endpoints /////
+
+// Add Comment
+app.post('/api/comment/add', ctrlComments.addComment)
+
+// Delete Comment
+app.delete('/api/comment/delete/:comment_id', ctrlComments.deleteCommment)
+
+// Get Comments
+app.get('/api/commments', ctrlComments.getCommments)
+
+// Edit Comment
+app.put('/api/commment/edit/:comment_id', ctrlComments.updateComment)
+
+///// Friend's Endpoints /////
+
+// Send Friend Request
+app.post('/api/friend_request/send/:user_id', ctrlFriends.sendRequest)
+
+// Confirm Friend
+app.put('/api/friend_request/confirm/:user_id', ctrlFriends.confirmRequest)
+
+// Delete Friend
+app.delete('/api/friend/delete/:user_id', ctrlFriends.deleteFriend)
+
+// Get Friends
+app.get('/api/friends', ctrlFriends.getFriends)
+
+///// Messages Endpoints /////
+
+// Send Message
+app.post('/api/message/send/:user_id', ctrlMessages.sendMessage)
+
+// Delete Message
+app.delete('/api/message/delete/:message_id', ctrlMessages.deleteMessage)
+
+// Get All Messages
+app.get('/api/messages', ctrlMessages.getMessages)
+
+// Get Message 
+app.get('/api/message/:message_id', ctrlMessages.getMessage)
+
+
 
 
 
