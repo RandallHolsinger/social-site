555800e52835e815aeea5f4d7e93ccef1c38ade8
Randall Holsinger <randallholsinger@gmail.com>
2022-07-17 17:31:35 -0700

    working on comment functionaily/ basic styling

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Post.scss'
-import Comments from '../Comments'
+import Comments from '../Comments/Comments'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComment, faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
 
 function Post(props) {
-  
+
+  const [showComments, setShowComments] = useState(false)
+
   return(
     <div className="Post">
       <article>
+        <header>
+          <span>
+            {props.value.profile_img ? <img src="" alt="profile" /> : <img src={'./images/user-image-default-black.svg'} alt="profile" /> }
+          </span>
+          <span>
+            {props.value.first_name}{' '}{props.value.last_name}
+          </span>
+        </header>
         <p>{props.value.post}</p>
+        <footer>
+          <span><FontAwesomeIcon icon={faThumbsUp} />{' '}Like</span>
+          <span><FontAwesomeIcon icon={faThumbsDown} />{' '}Dislikes</span>
+          <span onClick={() => setShowComments(!showComments)}><FontAwesomeIcon icon={faComment} />{' '}Comments</span>
+        </footer>
+        {showComments ?
+          <section>
+            <Comments postId={props.value.post_id} />
+          </section>
+        :
+        null
+        }
       </article>
-      <Comments />
     </div>
   )
 }
