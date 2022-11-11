7ac82dce94502fd4c423e6b9ece171efe306c6bf
Randall Holsinger <randallholsinger@gmail.com>
2022-11-10 20:19:57 -0800

    create friends page and functionality

--- db/friends/get_all_friends.sql
+++ db/friends/get_all_friends.sql
@@ -3,4 +3,5 @@ JOIN (SELECT user_id, first_name, last_name, profile_img FROM users) AS u ON (
  CASE WHEN f.source_id = ${user_id} THEN f.target_id 
          WHEN f.target_id = ${user_id} THEN f.source_id
     END) = u.user_id
-WHERE (f.source_id = ${user_id} OR f.target_id = ${user_id}) AND f.friend_status = 'friend'
+WHERE (f.source_id = ${user_id} OR f.target_id = ${user_id}) AND (f.friend_status = 'friend' OR f.friend_status = 'sent')
+ORDER BY f.friend_status = 'sent' desc 
--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,33 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Friend from '../Friend/Friend'
 
-function Friends(props) {
+function Friends() {
+
+  const [friends, setFriends] = useState([])
+
+  const getFriends = async () => {
+    try {
+      let res = await axios.get('/api/friends')
+      setFriends(res.data)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  let mappedFriends = friends.map(friend => {
+    return (
+      <Friend key={friend.friend_id} value={friend} />
+    )
+  })
+
+  useEffect(() => {
+    getFriends()
+  }, [])
+
   return (
     <div className="Friends">
-      Friends Here
+      {mappedFriends}
     </div>
   )
 }
