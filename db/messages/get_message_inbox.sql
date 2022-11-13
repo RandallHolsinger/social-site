6a59807044e3546da9e4ad1f8b73d4109ecefca2
Randall Holsinger <randallholsinger@gmail.com>
2022-11-12 17:10:33 -0800

    working on receiving correct message data

--- db/messages/get_message_inbox.sql
+++ db/messages/get_message_inbox.sql
@@ -1,14 +1,9 @@
--- SELECT * FROM messages as m
---   JOIN (SELECT user_id, first_name, last_name, profile_img FROM users) AS u
---   ON u.user_id = m.user_id_sender
--- WHERE (m.user_id_sender = ${user_id} OR m.user_id_receiver = ${user_id})
--- ORDER  BY date DESC
+SELECT *
+FROM (SELECT *,
+             row_number() OVER (PARTITION BY (CASE WHEN user_id_sender = ${user_id} THEN user_id_receiver ELSE user_id_sender END) ORDER BY date DESC) AS seqnum
+      FROM messages AS m
+      JOIN (SELECT user_id, first_name, last_name, profile_img FROM users) AS u ON m.user_id_sender = u.user_id
+      WHERE ${user_id} IN (user_id_sender, user_id_receiver)
+     ) messages
+WHERE seqnum = ${user_id}
 
-SELECT m.*
-FROM messages m 
-  JOIN (SELECT user_id, first_name, last_name, profile_img FROM users) AS u ON m.user_id_receiver = u.user_id
-WHERE ${user_id} IN (m.user_id_sender , m.user_id_receiver) AND
-      (LEAST(m.user_id_sender, m.user_id_receiver), GREATEST(m.user_id_sender, m.user_id_receiver), date) IN 
-       (SELECT LEAST(m2.user_id_sender, m2.user_id_receiver), GREATEST(m2.user_id_sender, m2.user_id_receiver), MAX(m2.date)
-        FROM messages m2
-        GROUP BY LEAST(m2.user_id_sender, m2.user_id_receiver), GREATEST(m2.user_id_sender, m2.user_id_receiver));
--- db/messages/send_message.sql
+++ db/messages/send_message.sql
@@ -1,14 +1,14 @@
 INSERT INTO messages(user_id_sender, user_id_receiver, subject, message, date)
 VALUES(${user_id_sender}, ${user_id_receiver}, ${subject}, ${message}, now());
 
-INSERT INTO inbox(message_id, user_id1, user_id2, last_message_user_id, subject, last_message, date)
-VALUES(lastval(), ${user_id_sender}, ${user_id_receiver}, ${user_id_sender}, ${subject}, ${message}, now())
-ON CONFLICT (message_id, user_id1, user_id2, last_message_user_id, subject, last_message, date)
-DO
-  UPDATE SET
-  message_id = lastval(), 
-  last_message_user_id = ${user_id_sender},
-  subject= ${subject},
-  last_message = ${message},
-  date = now();
+-- INSERT INTO inbox(message_id, user_id1, user_id2, last_message_user_id, subject, last_message, date)
+-- VALUES(lastval(), ${user_id_sender}, ${user_id_receiver}, ${user_id_sender}, ${subject}, ${message}, now())
+-- ON CONFLICT (message_id, user_id1, user_id2, last_message_user_id, subject, last_message, date)
+-- DO
+--   UPDATE SET
+--   message_id = lastval(), 
+--   last_message_user_id = ${user_id_sender},
+--   subject= ${subject},
+--   last_message = ${message},
+--   date = now();
 
