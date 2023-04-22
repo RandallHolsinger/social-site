3998f15edcd33fdd49aed8157c09334b3650bca4
Randall Holsinger <randallholsinger@gmail.com>
2023-04-21 17:25:36 -0700

    message styling

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './Message.scss'
 import FormatedDate from '../FormatedDate/FormatedDate'
 import FormatedTime from '../FormatedTime/FormatedTime'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 function Message(props) {
 
@@ -12,7 +14,11 @@ function Message(props) {
       <article>
         <header className='message-header'>
           <div className="message-profile-img-container">
-            <img src={`/uploads/images/${value.profile_img}`} alt='profile' />
+            {value.profile_img ? 
+              <img src={`/uploads/images/${value.profile_img}`} alt='profile' />
+            :
+              <FontAwesomeIcon icon={faUser} className='message-default-user-image'/>
+            }
           </div>
           <div className="message-details">
             <h3 className='message-sender-name'>{value.first_name}{' '}{value.last_name}</h3>
