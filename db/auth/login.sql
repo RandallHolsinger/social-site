3b217c276246a1cedc6953454ad16275e599ac45
Randall Holsinger <randallholsinger@gmail.com>
2023-05-05 19:14:43 -0700

    fixed authentication issue

--- db/auth/login.sql
+++ db/auth/login.sql
@@ -1,2 +1,1 @@
-SELECT user_id, first_name, last_name, profile_img, dob, city, state_province,
-occupation, high_school, college, about_me FROM users WHERE email = ${email}
+SELECT * FROM users WHERE email = ${email}
