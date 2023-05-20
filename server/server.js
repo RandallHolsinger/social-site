ca3d4bee4a267aa778c6ef7baf5bdfbb809eb6da
Randall Holsinger <randallholsinger@gmail.com>
2023-05-19 21:38:56 -0700

    fixing deployment issue

--- server/server.js
+++ server/server.js
@@ -30,7 +30,11 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use( express.static( `${__dirname}/../build` ) )
+app.use(express.static( `${__dirname}/../build` ) )
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const pgPool = new pg.Pool({
   connectionString: CONNECTION_STRING
