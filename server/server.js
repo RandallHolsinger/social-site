2a5e116b7d30e97de1da8ec28640d597ed771c2a
Randall Holsinger <randallholsinger@gmail.com>
2023-05-23 17:58:47 -0700

    debugging image upload

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.json())
 
 app.use(express.static( `${__dirname}/../build` ))
 
-app.use('*/image', express.static(`${__dirname}/../public`))
+app.use('*/image', express.static(`${__dirname}/../public/uploads/images`))
 
 const pgPool = new pg.Pool({
   connectionString: CONNECTION_STRING
