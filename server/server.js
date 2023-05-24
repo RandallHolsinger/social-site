1ce5f28ed98405f80f00b3e8d9f3608e6af60503
Randall Holsinger <randallholsinger@gmail.com>
2023-05-23 18:28:52 -0700

    debugging path for image location

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.json())
 
 app.use(express.static( `${__dirname}/../build` ))
 
-app.use('*/image', express.static('public/uploads/images'))
+app.use(express.static('public'))
 
 const pgPool = new pg.Pool({
   connectionString: CONNECTION_STRING
