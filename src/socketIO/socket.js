1f95b6f67fc86ab762a636ca51b26c513a39537c
Randall Holsinger <randallholsinger@gmail.com>
2023-08-07 20:54:19 -0700

    debugging socket.io on mobile

--- src/socketIO/socket.js
+++ src/socketIO/socket.js
@@ -1,7 +1,3 @@
 import socketIO from 'socket.io-client';
-const socket = socketIO.connect([
-  'http://localhost:3000/',
-  'https://socialyze.site',
-  'https://www.socialyze.site'
-], { autoConnect: false });
+const socket = socketIO.connect('https://www.socialyze.site', { autoConnect: false });
 export default socket
